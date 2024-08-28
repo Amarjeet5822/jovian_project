e194b7499d46db605640774cdd983d0860f86d82
Amarjeet Gupta <155504768+Amarjeet5822@users.noreply.github.com>
2024-08-28 08:08:36 +0000

    added flask project

---
+++ app.py
@@ -0,0 +1,10 @@
+from logging import debug
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+  return "Hello, Amarjeet"
+
+if __name__=="__main__":
+  app.run(host='0.0.0.0',debug=True)
+  
